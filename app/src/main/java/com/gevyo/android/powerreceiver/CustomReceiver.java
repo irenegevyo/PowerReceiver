ccc04817e4ac137161eba56e5621a79fdff257ac
irenegevyo <irenegevyo@gmail.com>
2019-04-04 16:24:56 +0700

    Initial commit

---
+++ app/src/main/java/com/gevyo/android/powerreceiver/CustomReceiver.java
@@ -0,0 +1,34 @@
+package com.gevyo.android.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class CustomReceiver extends BroadcastReceiver {
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+
+        if(intentAction !=null) {
+            String toastMessage = "unknown intent action";
+            switch (intentAction){
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "pwoer connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected";
+                    break;
+
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+
+        }
+    }
+}
